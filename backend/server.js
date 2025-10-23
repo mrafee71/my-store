f4f3151dc124e6a515d628180f003a90bdeea738
mrafee71 <muhammadrafeequeph@gmail.com>
2025-10-23 17:33:33 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -42,4 +42,6 @@ app.use('/api/admin/users', adminRoutes);
 app.use('/api/admin/products', productAdminRoutes);
 app.use('/api/admin/orders', adminOrderRoutes);
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
